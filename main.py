d0aa118eb045f0e54ea989435db1b1900f4c2587
John G. Nguyen <john.nguyen.career@gmail.com>
2021-04-02 02:27:18 -0500

    added print statements and change rules_engine module to rulesengine

--- main.py
+++ main.py
@@ -2,17 +2,44 @@
 
 import person
 import product
-import rules_engine
+import rulesengine
 		
 # MAIN body
+
+# Start
 aperson = person.Person(720, "Florida")
 aproduct = product.Product("7-1 ARM", 5.0, False)
 
+print("\n###")
+print('Person - state and credit score: {} and {}'.format(aperson.state, aperson.credit_score))
+print('Product - name and starting interest rate: {} and {}'.format(aproduct.name, aproduct.interest_rate))
+print("###\n")
+
 # Call loadRules() to populate a rules list
-rules = rules_engine.loadRules()
+rules = rulesengine.loadRules("rules_finance.csv")
 
 # Instantiate RulesEngine
-rules_engine = rules_engine.RulesEngine(aperson, aproduct, rules)
+rules_engine = rulesengine.RulesEngine(aperson, aproduct, rules)
 
 # Run the rules engine
 rules_engine.runRules()
+rules_engine.printOutput()
+
+# Start
+aperson = person.Person(500, "Texas")
+aproduct = product.Product("30 YEAR FIXED", 4.0, False)
+
+print("\n###")
+print('Person - state and credit score: {} and {}'.format(aperson.state, aperson.credit_score))
+print('Product - name and starting interest rate: {} and {}'.format(aproduct.name, aproduct.interest_rate))
+print("###\n")
+
+# Call loadRules() to populate a rules list
+rules = rulesengine.loadRules("rules_finance.csv")
+
+# Instantiate RulesEngine
+rules_engine = rulesengine.RulesEngine(aperson, aproduct, rules)
+
+# Run the rules engine
+rules_engine.runRules()
+rules_engine.printOutput()
